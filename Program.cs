14667aea0e79f7850c2b08bab17d61bd557598de
Emmanuel Marchal <81331382+irbt@users.noreply.github.com>
2022-11-28 16:24:04 +0100

    Works

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Program{
+class DrawTask{
+    public static void Main(){
+
+static void  Parallelogram(){
+
+for (int i = 4 ; i >= 0 ; i--){
+  int  j = i;
+  for(; j>= 0 ; j-- ){
+    Console.Write(" ");
+  }
+  Console.Write("***************");
+  Console.WriteLine();
+}
+}
+Parallelogram();
+    }
+}
+
+}
